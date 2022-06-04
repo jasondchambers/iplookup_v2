6c8847551ac41298c7f2e93f4335ea308555c43d
Jason Chambers <jason.d.chambers@gmail.com>
2022-06-04 12:06:59 -0400

    Fixed again

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 docker build --tag iplookupv2 .
 docker stop iplookupv2
 docker rm iplookupv2
-nohup docker run --restart unless-stopped --name iplookupv2 -p 5001:5001 iplooku
-pv2_image &
+nohup docker run --restart unless-stopped --name iplookupv2 -p 5001:5001 iplookupv2 &
